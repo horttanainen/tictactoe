197d59a34d9a4f7c4561a7279366f43f68475454
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-09-24 20:39:02 +0100

    Added matrix dimensions to make debugging easier

--- src/TicTacToe/AiInterface.hs
+++ src/TicTacToe/AiInterface.hs
@@ -98,9 +98,7 @@ type W1     = Matrix R
 type W2     = Matrix R
 type W3     = Matrix R
 type Output = Vector R
-type B1     = Vector R
-type B2     = Vector R
-type B3     = Vector R
+type B      = Matrix R
 
 type Hidden1  = Matrix R
 type Hidden2  = Matrix R
@@ -116,9 +114,9 @@ data Model = Model {
   w1        :: W1,
   w2        :: W2,
   w3        :: W3,
-  b1        :: B1,
-  b2        :: B2,
-  b3        :: B3
+  b1        :: B,
+  b2        :: B,
+  b3        :: B
 }
 
 newtype Target = Target {
@@ -210,21 +208,25 @@ calcTarget (BufferD state action reward nextState done) = do
 fitModel :: Input -> Output -> Network ()
 fitModel state target = do
   m@(Model w1 w2 w3 b1 b2 b3) <- gets model
-  let hidden1 = relu $ add b1 $ w1 #> state
-      hidden2 = relu $ add b2 $ w2 #> hidden1
-      prediction = add b3 $ w3 #> hidden2
-      loss = meanSquaredError target prediction
-      g1 = w1 #> state + b1
-      g2 = w2 #> hidden1 + b2
-      dg3 = - (target - prediction)
-      dg2 = fromList ((\el -> if el <= 0 then 0 else 1) <$> toList g2) * dg3 <# w3
-      dg1 = fromList ((\el -> if el <= 0 then 0 else 1) <$> toList g1) * dg2 <# w2
-      dw3 = dg3 * hidden2
-      db3 = dg3
-      dw2 = dg2 * hidden1
-      db2 = dg2
-      dw1 = dg1 * state
-      db1 = dg1
+  let state' = asColumn state -- [9x1]
+      target' = asColumn target -- [9x1]
+      hidden1 = relu $ b1 + (w1 <> state') -- [24x9] x [9x1] = [24x1]
+      hidden2 = relu $ add b2 $ w2 <> hidden1 -- [24x24] x [24x1] = [24x1]
+      prediction = add b3 $ w3 <> hidden2 -- [9x24] x [24x1] = [9x1]
+      loss = meanSquaredError target' prediction -- [9x1]
+      g1 = w1 <> state' + b1 -- [24x9] x [9x1] = [24x1]
+      g2 = w2 <> hidden1 + b2 -- [24x24] x [24x1] = [24x1]
+      dg3 = - (target' - prediction) -- [9x1]
+      dg2 = drelu g1 * (tr' dg3 <> w3) -- [1x9] x [9x24] = [1x24]
+      dg1 = drelu g2 * dg2 <> w2 -- [1x24] x [24x24] = [1x24]
+      dw3 = dg3 <> tr' hidden2 -- [9x1] x [1x24] = [9x24]
+      db3 = dg3 -- [9x1]
+      dw2 = tr' dg2 <> tr' hidden1 -- [24x1] x [1x24] = [24x24]
+      db2 = tr' dg2 -- [24x1]
+      dw1 = tr' dg1 <> tr' state' -- [24x1] x [1x9] = [24x9]
+      db1 = tr' dg1 -- [24x1]
+      drelu g = fromLists $ map (map cutOff) $ toLists g 
+      cutOff el = if el <= 0 then 0 else 1
       
   return ()
 
@@ -255,9 +257,10 @@ randomAction = do
 
 predict :: Model -> Input -> Network Output
 predict (Model w1 w2 w3 b1 b2 b3) input = do
-  let hidden1 = relu $ add b1 $ w1 #> input
-      hidden2 = relu $ add b2 $ w2 #> hidden1
-  return (add b3 $ w3 #> hidden2)
+  let input' = asColumn input -- [9x1]
+      hidden1 = relu $ add b1 $ w1 <> input' -- [24x9] x [9x1] = [24x1]
+      hidden2 = relu $ add b2 $ w2 <> hidden1 -- [24x24] x [24x1] = [24x1]
+  return (flatten $ add b3 $ w3 <> hidden2) -- [9x24] x [24x1] = [9x1]
 
 remember :: BufferData -> Network ()
 remember bData = do
@@ -270,10 +273,11 @@ remember bData = do
 adam :: Network ()
 adam = assert False undefined
 
-relu :: Activator
-relu vec =
-  fromList $ map relu $ toList vec
+relu :: Matrix R -> Matrix R
+relu m =
+  fromLists $ map (map relu) rows
     where
+      rows = toLists m
       relu = max 0
 
 initHyp :: HyperParameters
